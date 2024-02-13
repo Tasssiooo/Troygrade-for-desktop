86ccc2829830d208bdba3f44b9b7d9a33d5321fb
Tasssiooo <tassioribeirofontes@gmail.com>
2024-02-12 22:34:58 -0300

    File picker set

--- src/components/SideBar/components/Actions/ActionButtons.tsx
+++ src/components/SideBar/components/Actions/ActionButtons.tsx
@@ -1,7 +1,22 @@
 import * as Toolbar from "@radix-ui/react-toolbar";
 import Tooltip from "@/components/Globals/Tooltip";
 
+import { open } from "@tauri-apps/api/dialog";
+
 export default function ActionButtons() {
+  async function openExplorer() {
+    const selected = await open({
+      multiple: true,
+      filters: [
+        {
+          name: "Troybin",
+          extensions: ["troybin", "troy"],
+        },
+      ],
+      title: "Open .troybin file"
+    });
+  }
+
   return (
     <div className="flex flex-row items-center">
       <Tooltip tip="Filter" orientation="bottom">
@@ -11,7 +26,7 @@ export default function ActionButtons() {
       </Tooltip>
       <Tooltip tip="Add files" orientation="bottom">
         <Toolbar.Button asChild>
-          <button type="button" className="action add"></button>
+          <button type="button" className="action add" onClick={openExplorer}></button>
         </Toolbar.Button>
       </Tooltip>
     </div>
