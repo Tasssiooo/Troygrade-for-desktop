40ff6f8ad4f2bd3fadf50c211bb6191c0e21d422
Tasssiooo <tassioribeirofontes@gmail.com>
2024-02-13 18:56:17 -0300

    Tooltip UI done

--- src/components/Globals/Tooltip.tsx
+++ src/components/Globals/Tooltip.tsx
@@ -1,49 +1,48 @@
-import * as RadixTooltip from "@radix-ui/react-tooltip";
-import { ReactNode } from "react";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import { forwardRef } from "react";
 
-import "./styles/tooltip.css";
+import { cn } from "@/lib/utils";
 
-type Orientation = "bottom" | "top" | "right" | "left" | undefined;
+export const TooltipProvider = TooltipPrimitive.Provider;
 
-export default function Tooltip({
-  children,
-  orientation,
-  tip,
-}: {
-  children: ReactNode;
-  orientation: Orientation;
-  tip: string;
-}) {
-  return (
-    <RadixTooltip.Provider delayDuration={0}>
-      <RadixTooltip.Root>
-        <RadixTooltip.Trigger asChild>{children}</RadixTooltip.Trigger>
-        <RadixTooltip.Portal>
-          <RadixTooltip.Content className="TooltipContent" side={orientation}>
-            {tip}
-            <RadixTooltip.Arrow asChild>
-              <svg
-                version="1.2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 32 22"
-                width="22"
-                height="12"
-              >
-                <defs>
-                  <image
-                    width="30"
-                    height="22"
-                    id="img1"
-                    href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAWCAMAAADgvdz9AAAAAXNSR0IB2cksfwAAAIRQTFRFAAAAeFgocFQnKyciGhwhJSQialEndlkpeVkpeVopeFoobFInICAiVEMleFoogGAwVEIlQjckeFwoeFgoQzckclYneFgoeFopYEomeFooeVopSDoleFooMSsiJSMieFooeVkpSDokdlkmeFsoHyAid1knZk4nTj4leVspdlkneFooeFkoA5NeiwAAACx0Uk5TACDP////31Bw7/////+fEP//QGD//4CQ/9/P/8D//2BQ/1C//5D//+9wgKAKEflxAAAAuklEQVR4nHWQ2RKCMBAEJ4oHi4KiKKjgjdf//5+7WGAu5iXJTqW7agGowTDwZDSegDMNKZp56jlRrJCERLRw2yWPKQVWcq4ddMbTjdC30tv6nGeFtEh2fLX0e/mimhpK9LmBphYtcfQRvw/o0uhLA12of41GX+noo9aaekHHMHKSH2c/2tCXcrnAzpWnWRXcBH132p++Dh7duqykQq21dfn0+ro8+h50oy+4ffrRrf7V2wJve11WPib6C9FVDd2GPvG/AAAAAElFTkSuQmCC"
-                  />
-                </defs>
-                <style></style>
-                <use id="Background" href="#img1" x="1" y="0" />
-              </svg>
-            </RadixTooltip.Arrow>
-          </RadixTooltip.Content>
-        </RadixTooltip.Portal>
-      </RadixTooltip.Root>
-    </RadixTooltip.Provider>
-  );
-}
+export const Tooltip = TooltipPrimitive.Root;
+
+export const TooltipTrigger = TooltipPrimitive.Trigger;
+
+export const TooltipContent = forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+>(({ className, children, sideOffset = 4, ...props }, ref) => (
+  <TooltipPrimitive.Content
+    ref={ref}
+    sideOffset={sideOffset}
+    className={cn(
+      "relative z-[90] bg-[#1a1c21] leading-[1] min-w-12 text-center px-2 py-1 text-sm border [border-image:linear-gradient(to_top,#785b28_0%,#c89c3c_55%,#c8a355_71%,#c8aa6e_100%)_1_stretch]",
+      className
+    )}
+    {...props}
+  >
+    {children}
+    <TooltipPrimitive.Arrow asChild>
+      <svg
+        version="1.2"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 32 22"
+        width="22"
+        height="12"
+      >
+        <defs>
+          <image
+            width="30"
+            height="22"
+            id="img1"
+            href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAWCAMAAADgvdz9AAAAAXNSR0IB2cksfwAAAIRQTFRFAAAAeFgocFQnKyciGhwhJSQialEndlkpeVkpeVopeFoobFInICAiVEMleFoogGAwVEIlQjckeFwoeFgoQzckclYneFgoeFopYEomeFooeVopSDoleFooMSsiJSMieFooeVkpSDokdlkmeFsoHyAid1knZk4nTj4leVspdlkneFooeFkoA5NeiwAAACx0Uk5TACDP////31Bw7/////+fEP//QGD//4CQ/9/P/8D//2BQ/1C//5D//+9wgKAKEflxAAAAuklEQVR4nHWQ2RKCMBAEJ4oHi4KiKKjgjdf//5+7WGAu5iXJTqW7agGowTDwZDSegDMNKZp56jlRrJCERLRw2yWPKQVWcq4ddMbTjdC30tv6nGeFtEh2fLX0e/mimhpK9LmBphYtcfQRvw/o0uhLA12of41GX+noo9aaekHHMHKSH2c/2tCXcrnAzpWnWRXcBH132p++Dh7duqykQq21dfn0+ro8+h50oy+4ffrRrf7V2wJve11WPib6C9FVDd2GPvG/AAAAAElFTkSuQmCC"
+          />
+        </defs>
+        <style></style>
+        <use id="Background" href="#img1" x="1" y="0" />
+      </svg>
+    </TooltipPrimitive.Arrow>
+  </TooltipPrimitive.Content>
+));
+TooltipContent.displayName = TooltipPrimitive.Content.displayName;
--- src/components/SideBar/components/Actions/ActionButtons.tsx
+++ src/components/SideBar/components/Actions/ActionButtons.tsx
@@ -1,5 +1,10 @@
 import * as Toolbar from "@radix-ui/react-toolbar";
-import Tooltip from "@/components/Globals/Tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/Globals/Tooltip";
 
 import { open } from "@tauri-apps/api/dialog";
 
@@ -13,23 +18,41 @@ export default function ActionButtons() {
           extensions: ["troybin", "troy"],
         },
       ],
-      title: "Open .troybin file"
+      title: "Open .troybin file",
     });
     console.log(selected);
   }
 
   return (
     <div className="flex flex-row items-center">
-      <Tooltip tip="Filter" orientation="bottom">
-        <Toolbar.Button asChild>
-          <button type="button" className="action search"></button>
-        </Toolbar.Button>
-      </Tooltip>
-      <Tooltip tip="Add files" orientation="bottom">
-        <Toolbar.Button asChild>
-          <button type="button" className="action add" onClick={openExplorer}></button>
-        </Toolbar.Button>
-      </Tooltip>
+      <TooltipProvider delayDuration={0}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Toolbar.Button asChild>
+              <button type="button" className="action search"></button>
+            </Toolbar.Button>
+          </TooltipTrigger>
+          <TooltipContent sideOffset={0} side="bottom">
+            Filter
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <TooltipProvider delayDuration={0}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Toolbar.Button asChild>
+              <button
+                type="button"
+                className="action add"
+                onClick={openExplorer}
+              ></button>
+            </Toolbar.Button>
+          </TooltipTrigger>
+          <TooltipContent sideOffset={0} side="bottom">
+            Add files
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </div>
   );
 }
