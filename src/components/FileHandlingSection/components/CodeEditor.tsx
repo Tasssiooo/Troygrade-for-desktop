95252dcf96fb277ea733d6af48de7ce599b5851a
Tasssiooo <tassioribeirofontes@gmail.com>
2024-02-14 15:07:04 -0300

    Code editor of the main window changed to monaco

--- src/components/FileHandlingSection/components/CodeEditor.tsx
+++ src/components/FileHandlingSection/components/CodeEditor.tsx
@@ -1,21 +1,13 @@
-import Editor from "react-simple-code-editor";
-import { highlight, languages } from "prismjs";
-
-import { useState } from "react";
-
-import "./Handlers/prism.css";
+import Editor from "@monaco-editor/react";
 
 export default function CodeEditor() {
-  const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`);
   return (
     <Editor
-      className="font-['Fira_code','Fira_Mono',monospace] h-[411px] text-sm text-foreground bg-background"
-      value={code}
-      onValueChange={(code) => setCode(code)}
-      //The code in this editor isn't javascript, but I set it to javascript
-      //to make the prismjs hightlight puts beautiful colors in it. :))
-      highlight={(code) => highlight(code, languages.javascript, "javascript")}
-      padding={14}
+      width="100%"
+      height="413px"
+      theme="vs-dark"
+      defaultLanguage="ini"
+      defaultValue="function add(a, b) { return a + b; }"
     />
   );
 }
