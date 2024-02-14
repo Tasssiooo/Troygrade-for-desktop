77af56461db955bbff134d69b025092f025ebb55
Tasssiooo <tassioribeirofontes@gmail.com>
2024-02-14 15:10:43 -0300

    Tooltip arrow svg moved  to Icons components

--- src/components/Globals/Tooltip.tsx
+++ src/components/Globals/Tooltip.tsx
@@ -2,6 +2,7 @@ import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 import { forwardRef } from "react";
 
 import { cn } from "@/lib/utils";
+import { TooltipArrow } from "./Icons";
 
 export const TooltipProvider = TooltipPrimitive.Provider;
 
@@ -24,24 +25,7 @@ export const TooltipContent = forwardRef<
   >
     {children}
     <TooltipPrimitive.Arrow asChild>
-      <svg
-        version="1.2"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 32 22"
-        width="22"
-        height="12"
-      >
-        <defs>
-          <image
-            width="30"
-            height="22"
-            id="img1"
-            href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAWCAMAAADgvdz9AAAAAXNSR0IB2cksfwAAAIRQTFRFAAAAeFgocFQnKyciGhwhJSQialEndlkpeVkpeVopeFoobFInICAiVEMleFoogGAwVEIlQjckeFwoeFgoQzckclYneFgoeFopYEomeFooeVopSDoleFooMSsiJSMieFooeVkpSDokdlkmeFsoHyAid1knZk4nTj4leVspdlkneFooeFkoA5NeiwAAACx0Uk5TACDP////31Bw7/////+fEP//QGD//4CQ/9/P/8D//2BQ/1C//5D//+9wgKAKEflxAAAAuklEQVR4nHWQ2RKCMBAEJ4oHi4KiKKjgjdf//5+7WGAu5iXJTqW7agGowTDwZDSegDMNKZp56jlRrJCERLRw2yWPKQVWcq4ddMbTjdC30tv6nGeFtEh2fLX0e/mimhpK9LmBphYtcfQRvw/o0uhLA12of41GX+noo9aaekHHMHKSH2c/2tCXcrnAzpWnWRXcBH132p++Dh7duqykQq21dfn0+ro8+h50oy+4ffrRrf7V2wJve11WPib6C9FVDd2GPvG/AAAAAElFTkSuQmCC"
-          />
-        </defs>
-        <style></style>
-        <use id="Background" href="#img1" x="1" y="0" />
-      </svg>
+      <TooltipArrow />
     </TooltipPrimitive.Arrow>
   </TooltipPrimitive.Content>
 ));
