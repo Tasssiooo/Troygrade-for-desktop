7344b032b3e7438420e7975f6ba18cd07f9097ba
Tasssiooo <tassioribeirofontes@gmail.com>
2024-03-21 17:54:11 -0300

    Sonner options chnages

--- src/components/Globals/Sonner.tsx
+++ src/components/Globals/Sonner.tsx
@@ -9,14 +9,15 @@ const Toaster = ({ ...props }: ToasterProps) => {
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+            "group toast rounded-none border border-transparent [border-image:linear-gradient(to_top,#785a28_0,#463714_50%,#463714_100%)_1_stretch] group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
           description: "group-[.toast]:text-muted-foreground",
           actionButton:
-            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+            "bg-button text-button-foreground border border-transparent [border-image:linear-gradient(to_top,#785a28_0,#463714_50%,#463714_100%)_1_stretch]",
           cancelButton:
-            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+            "group-[.toast]:text-foreground group-[.toast]:text-foreground-muted",
         },
       }}
+      duration={3500}
       {...props}
     />
   );
