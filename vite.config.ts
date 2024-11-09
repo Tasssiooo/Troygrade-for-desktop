b5e6194d8cbc0c93c4b2dd7d8208f06a801bd846
Tasssiooo <tassioribeirofontes@proton.me>
2024-11-08 21:18:42 -0300

    Added alias to ./src/

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -28,5 +29,10 @@ export default defineConfig(async () => ({
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      }
+    }
   },
 }));
