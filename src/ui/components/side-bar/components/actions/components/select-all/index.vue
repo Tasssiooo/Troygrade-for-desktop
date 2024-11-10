ed6ea045cc4a0749abf609fa273f3bb184425595
Tasssiooo <tassioribeirofontes@proton.me>
2024-11-09 23:40:29 -0300

    Value prop set

--- src/ui/components/side-bar/components/actions/components/select-all/index.vue
+++ src/ui/components/side-bar/components/actions/components/select-all/index.vue
@@ -6,7 +6,7 @@ import { group, item } from "./styles.ts";
 
 <template>
   <ToolbarToggleGroup :class="group" type="single">
-    <ToolbarToggleItem :class="item + ' group'">
+    <ToolbarToggleItem :class="item + ' group'" value="_">
       <div :class="toggle({ turned: 'off' })" /> Select All
     </ToolbarToggleItem>
   </ToolbarToggleGroup>
