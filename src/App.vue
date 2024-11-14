ff0e8e85b233e463d184a18121e2b79edc97c25b
Tasssiooo <tassioribeirofontes@proton.me>
2024-11-14 01:36:25 -0300

    Toaster added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import ControllersBar from "./ui/components/controllers-bar/index.vue";
 import SideBar from "./ui/components/side-bar/index.vue";
+import Toaster from "./ui/components/toaster/index.vue";
 </script>
 
 <template>
   <ControllersBar />
   <main>
     <SideBar />
+    <Toaster />
   </main>
 </template>
---
+++ src/ui/components/toaster/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Toaster } from "vue-sonner";
+import { toast, description, close_button, error_icon } from "./styles";
+
+import MdiError from "../icons/mdi-error.vue";
+</script>
+
+<template>
+  <Toaster
+    expand
+    close-button
+    :toast-options="{
+      unstyled: true,
+      classes: {
+        toast,
+        description,
+        closeButton: close_button,
+      },
+    }"
+    :duration="3500"
+    :visibleToasts="5"
+  >
+    <template #error-icon>
+      <MdiError :class="error_icon" />
+    </template>
+  </Toaster>
+</template>
