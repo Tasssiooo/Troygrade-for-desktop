129589064fac89f46d6dcfd9bdd907b9dbf5693d
Tasssiooo <tassioribeirofontes@proton.me>
2024-11-14 15:20:57 -0300

    Filter functionality done

--- src/ui/components/side-bar/components/actions/components/filter/index.vue
+++ src/ui/components/side-bar/components/actions/components/filter/index.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 import { ToolbarButton } from "radix-vue";
+
 import { button } from "../../../../../../../../styled-system/recipes";
+
+import appState from "../../../../../../../states";
 </script>
 
 <template>
-  <ToolbarButton :class="button({ visual: 'mask', mask: 'filter' })" />
+  <ToolbarButton
+    :class="button({ visual: 'mask', mask: 'filter' })"
+    @click="() => appState.filter.show = !appState.filter.show"
+  />
 </template>
--- src/ui/components/side-bar/components/filter/index.vue
+++ src/ui/components/side-bar/components/filter/index.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 import { filter_area, filter, label } from "./styles";
+
+import appState from "../../../../../states";
+
+import { onUpdated } from "vue";
+
+onUpdated(() => {
+  document.getElementById("filter")?.focus();
+});
 </script>
 
 <template>
-  <div :class="filter_area">
+  <div v-if="appState.filter.show" :class="filter_area">
     <label :class="label" for="filter" />
     <input
       id="filter"
@@ -11,7 +19,13 @@ import { filter_area, filter, label } from "./styles";
       name="filter"
       placeholder="Filter"
       autocomplete="off"
+      :value="appState.filter.value"
       :class="filter"
+      @change="
+        /* Delete this (e.target as HTMLTextAreaElement) and make typescript cry. */
+        (e) => (appState.filter.value = (e.target as HTMLTextAreaElement).value)
+      "
+      @blur="appState.filter.show = false"
     />
   </div>
 </template>
