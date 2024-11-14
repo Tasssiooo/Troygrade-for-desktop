379b212931239680010ab44fffa2e523d49830b3
Tasssiooo <tassioribeirofontes@proton.me>
2024-11-14 14:20:35 -0300

    Add files action done

--- src/ui/components/side-bar/components/actions/components/add-files/index.vue
+++ src/ui/components/side-bar/components/actions/components/add-files/index.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { ToolbarButton } from "radix-vue";
+
 import { button } from "../../../../../../../../styled-system/recipes";
+
+import { handleLoadFiles } from "../../../../../../../lib/handlers";
 </script>
 
 <template>
-  <ToolbarButton :class="button({ visual: 'mask', mask: 'add' })" />
+  <ToolbarButton :class="button({ visual: 'mask', mask: 'add' })" @click="handleLoadFiles" />
 </template>
