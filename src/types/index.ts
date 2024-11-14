c14c21e772821fa2263400851bca9de7cc4b3ee5
Tasssiooo <tassioribeirofontes@proton.me>
2024-11-14 14:08:52 -0300

    Global types

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type FileType =
+  | "MIG_BIN"
+  | "CONV_TROYBIN"
+  | "CONV_BIN"
+  | "TROYBIN"
+  | "UNKNOWN_FILE_TYPE";
+
+export interface Entry {
+  id: string;
+  error?: string;
+  name: string;
+  content?: ArrayBuffer | Uint8Array | string;
+  type: FileType;
+}
+
+export interface Settings {
+  assetsPath: string;
+  filePath: string;
+  namesOnly: boolean;
+  settingsPreset: string;
+  updateFileTypes: boolean;
+}
