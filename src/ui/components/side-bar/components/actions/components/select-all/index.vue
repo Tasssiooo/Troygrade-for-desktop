c8566a80929e4caa6977438fb8c001e0a263eb45
Tasssiooo <tassioribeirofontes@proton.me>
2024-11-15 16:40:38 -0300

    Select all toggle functionality done

--- src/ui/components/side-bar/components/actions/components/select-all/index.vue
+++ src/ui/components/side-bar/components/actions/components/select-all/index.vue
@@ -1,13 +1,39 @@
 <script setup lang="ts">
 import { ToolbarToggleGroup, ToolbarToggleItem } from "radix-vue";
+
 import { toggle } from "../../../../../../../../styled-system/recipes";
+
 import { group, item } from "./styles.ts";
+
+import appState from "../../../../../../../states/index.ts";
+
+function handleSelectAll() {
+  if (appState.selectedFiles.length !== appState.files.length) {
+    appState.selectedFiles = appState.files.map((file) => file.id);
+  } else {
+    appState.selectedFiles = [];
+  }
+}
 </script>
 
 <template>
-  <ToolbarToggleGroup :class="group" type="single">
+  <ToolbarToggleGroup
+    :class="group"
+    type="single"
+    :onUpdate:modelValue="handleSelectAll"
+  >
     <ToolbarToggleItem :class="item + ' group'" value="_">
-      <div :class="toggle({ turned: 'off' })" />
+      <div
+        :class="
+          toggle({
+            turned:
+              appState.files.length &&
+              appState.files.length === appState.selectedFiles.length
+                ? 'on'
+                : 'off',
+          })
+        "
+      />
       Select All
     </ToolbarToggleItem>
   </ToolbarToggleGroup>
