297cc3f510ac86ba1f5842c333d37d3526f32e50
Tasssiooo <tassioribeirofontes@proton.me>
2024-11-14 21:04:39 -0300

    New icon

---
+++ src/ui/components/icons/mdi-checkbox-marked-circle.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="m10 17l-5-5l1.41-1.42L10 14.17l7.59-7.59L19 8m-7-6A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2"
+    />
+  </svg>
+</template>
