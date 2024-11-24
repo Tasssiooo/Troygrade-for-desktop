c6a4447e7746f32ac117b3a58f167299434fae3f
Tasssiooo <tassioribeirofontes@proton.me>
2024-11-24 17:30:38 -0300

    Main window added

---
+++ src/ui/windows/main.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SideBar from "../../ui/components/side-bar/index.vue";
+import CodeEditor from "../../ui/components/code-editor/index.vue";
+</script>
+
+<template>
+  <SideBar />
+  <CodeEditor />
+</template>
