419b16dc1c300b61eb839033ada838e850c8eff5
Tasssiooo <tassioribeirofontes@proton.me>
2024-11-24 17:13:39 -0300

    New icons added

---
+++ src/ui/components/icons/mdi-delete-outline.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6zM8 9h8v10H8zm7.5-5l-1-1h-5l-1 1H5v2h14V4z"
+    />
+  </svg>
+</template>
