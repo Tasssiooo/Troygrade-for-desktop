e53240886eefe54d18a57020051d35835493c90a
Tasssiooo <tassioribeirofontes@proton.me>
2024-11-30 00:10:41 -0300

    Fixed zIndex and padding

--- src/ui/components/tooltip/styles.ts
+++ src/ui/components/tooltip/styles.ts
@@ -1,10 +1,11 @@
 import { css } from "../../../../styled-system/css";
 
 const content = css({
+  zIndex: "90",
   border: "1px solid transparent",
+  p: "1.5",
   fontSize: "sm",
   bg: "#1a1c21",
-  p: "1.5",
   borderImage:
     "linear-gradient(to top,#785b28 0%,#c89c3c 55%,#c8a355 71%,#c8aa6e 100%) 1 stretch",
 });
